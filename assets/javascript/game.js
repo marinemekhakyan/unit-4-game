22c6365de4974dfb921ecd1543906cd38cdd4463
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-07-05 17:26:14 -0700

    assigned global variables in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    //Random number arrays
+
+    //Computer choice
+
+    let randomN = [];
+
+    for (let i = 19; i < 121; i ++) {
+        randomN.push(i);
+    }
+    //Crystals - user choice
+    let crystals = [];
+
+    for (let c = 1; c < 13; c++) {
+        crystals.push(c);
+    }
+
+    let randomNum;
+    let crystalNum = [];
+
+    let c1;
+    let c2; 
+    let c3;
+    let c4;
+
+    let totalScore = 0;
+
+    let wins = 0;
+    let losses = 0;
+
+ 
+
+})
